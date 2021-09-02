307ba4386ca55c7f07926b28e1c6deb6a8190caf
LeanC1000 <leandrococa1999@gmail.com>
2021-09-02 14:45:19 -0300

    envio de datos del template al view

--- comparation/views.py
+++ comparation/views.py
@@ -9,21 +9,32 @@ import requests
 def home(request):
     #my_url = request.POST.get("url")
     #my_device = request.POST.get("device")
-    #APIKey = "AIzaSyALIsdCRDWuduGIy071_x5lOFsVgyq3WaE"
+    APIKey = "AIzaSyALIsdCRDWuduGIy071_x5lOFsVgyq3WaE"
+    my_url = request.POST.get("url")
+    value = True
 
-    url = "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key=AIzaSyBx49mLr1xROPyOK7gXa_wtulSRkedGPd8"
+# https://developers.google.com
+#    url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={my_url}&key={APIKey}"
+    if len(my_url)== 0:
+      value = True
+      context = {
+          "value": value,
+          "my_url": my_url,
+      }
+    else:
+      value=False
+      url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key={APIKey}"
+      # API request
+      result = urllib.request.urlopen(url.format(url)).read().decode('UTF-8')
+      result_json = json.loads(result)
 
-    # API request
-    result = urllib.request.urlopen(url.format(url)).read().decode('UTF-8')
-    result_json = json.loads(result)
-
-    print(result_json["lighthouseResult"]["audits"]["cumulative-layout-shift"]["displayValue"])
-    context = {
-        "interactive": result_json["lighthouseResult"]["audits"]["interactive"],
-        "speed_index": result_json["lighthouseResult"]["audits"]["speed-index"]
-        
-    }
-
+      context = {
+          "my_url": my_url,
+          "value": value,
+          "interactive": result_json["lighthouseResult"]["audits"]["interactive"],
+          "speed_index": result_json["lighthouseResult"]["audits"]["speed-index"]
+      }
+      
     return render(request, 'home.html', context)
 
 
