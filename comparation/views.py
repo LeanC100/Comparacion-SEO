de8a6455407642979c28ccbb5eaaa32d4d984d67
LeanC1000 <leandrococa1999@gmail.com>
2021-09-02 22:12:07 -0300

    separacion de los sitios con el objetivo de que no se superpongan los valores

--- comparation/views.py
+++ comparation/views.py
@@ -7,43 +7,53 @@ import requests
 # Create your views here.
 
 def home(request):
-    #my_url = request.POST.get("url")
-    #my_device = request.POST.get("device")
-    APIKey = "AIzaSyALIsdCRDWuduGIy071_x5lOFsVgyq3WaE"
-    my_url = request.POST.get("url")
-    value = True
 
-# https://developers.google.com
-#    url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={my_url}&key={APIKey}"
-    if len(my_url)== 0:
-      value = True
-      context = {
-          "value": value,
-          "my_url": my_url,
-      }
-    else:
-      value=False
-      url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://developers.google.com&key={APIKey}"
-      # API request
-      result = urllib.request.urlopen(url.format(url)).read().decode('UTF-8')
-      result_json = json.loads(result)
+    my_url1 = request.POST.get("url1")
+    my_url2 = request.POST.get("url2")
 
-      context = {
-          "my_url": my_url,
-          "value": value,
-          "interactive": result_json["lighthouseResult"]["audits"]["interactive"],
-          "speed_index": result_json["lighthouseResult"]["audits"]["speed-index"]
-      }
-      
+    date1 = date_site(my_url1)
+    date2 = date_site(my_url2)
+
+
+    context={
+      "date1":date1,
+      "date2": date2
+    }
     return render(request, 'home.html', context)
-
-
-
-
-
     #response = requests.get(
       #  '
      #   ).json()
 
    # return render(request, 'home.html',  {'response': response})
 
+
+def date_site(value):
+    #my_url = request.POST.get("url")
+    #my_device = request.POST.get("device")
+    APIKey = "AIzaSyALIsdCRDWuduGIy071_x5lOFsVgyq3WaE"
+    my_url = value
+    url_status = True
+
+# https://developers.google.com
+#    url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url={my_url}&key={APIKey}"
+    if my_url is None:
+      url_status = True
+      context = {
+          "url_status": url_status,
+          "my_url": my_url,
+      }
+    else:
+      url_status = False
+      url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://google.com&key={APIKey}"
+      # API request
+      result = urllib.request.urlopen(url.format(url)).read().decode('UTF-8')
+      result_json = json.loads(result)
+
+      context = {
+          "my_url": my_url,
+          "url_status": url_status,
+          "interactive": result_json["lighthouseResult"]["audits"]["interactive"],
+          "speed_index": result_json["lighthouseResult"]["audits"]["speed-index"]
+      }
+    
+    return context
